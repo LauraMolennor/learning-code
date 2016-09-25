3a06121d82f56815ad616efcfaec2f15d951e094
Laura Molennor <lauramolennor@lauras-macbook-pro.local>
2016-09-25 15:39:32 -0400

    Initial Commit

---
+++ InstagramApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstagramApp
+//
+//  Created by Laura Molennor on 9/25/16.
+//  Copyright © 2016 Molennor Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
